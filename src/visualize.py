e593ab0b72f13129a48eeeb664312ebaaf4b98d9
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-07-21 19:35:31 -0400

    [ADD] src/visualize

---
+++ src/visualize.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def price_date():
+    fig, ax = plt.subplots(figsize=(3.,2.3))
+    return fig
+
+def price_day():
+    fig, ax = plt.subplots(figsize=(3.,2.3))
+    return fig
+
+def price_hour():
+    fig, ax = plt.subplots(figsize=(3.,2.3))
+    return fig
