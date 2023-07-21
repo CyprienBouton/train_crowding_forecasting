b98d37978120f6b4df75628f53c6d1d842c5973e
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-07-21 19:36:22 -0400

    [UP] \streamlit_pages\page3 add figure

--- streamlit_pages/page3.py
+++ streamlit_pages/page3.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from src.visualize import (price_day, price_date, price_hour)
 
 def page3():
     st.markdown("<h1>Data Visualization</h1>", unsafe_allow_html=True)
+    parameter_to_visualized = {
+        "Date": price_date,
+        "Day": price_day,
+        "Hour": price_hour,
+    }
+
+    selected_visualization = st.selectbox("Choose a parameter:", parameter_to_visualized.keys())
+    st.pyplot(parameter_to_visualized[selected_visualization]())
