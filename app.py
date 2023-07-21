e9ce6bbc1d7e75c4ca9e89159d232735eae78429
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-07-21 19:26:09 -0400

    [UP] app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-from sklearn.linear_model import LinearRegression
-from src.evaluate_model import evaluate_model
-from src.load_data import load_data
-from src.preprocess import preprocessing
-
-X, y, _ = load_data()
-preprocessing(X)
-
 import streamlit as st
 from streamlit_pages import main_page, page2, page3, page4
 
