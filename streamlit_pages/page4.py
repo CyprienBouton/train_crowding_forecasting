c9dcd9ed2dedb5bcfdfa1d2bc7305869fdc11db3
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-20 21:57:21 -0400

    [ADD] contexual variables in streamlit_pages/page4

--- streamlit_pages/page4.py
+++ streamlit_pages/page4.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.svm import SVR
@@ -6,8 +7,11 @@ from sklearn.tree import ExtraTreeRegressor, DecisionTreeClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 
+from src.load_data import load_data
 
 def page4():
+
+    X_train, _, _ = load_data()
 
     class_model_names = {
         'Linear Model': LinearRegression,
@@ -17,7 +21,27 @@ def page4():
         'Random Forest': RandomForestRegressor,
         'SVR': SVR
     }
-    
+
     st.markdown("<h1>Occupancy rate forecasting</h1>", unsafe_allow_html=True)
     model_used = st.selectbox("What model would you like to use?",
                               class_model_names.keys())
+    
+    # Contextual variables
+    date = st.date_input('Date of train passage', 
+                  value= datetime(2019,1,7), 
+                  min_value=datetime(2019,1,7), 
+                  max_value=datetime(2019,5,20), 
+                  help="Date must be a weekday between January 7th and May 20th")
+    
+    train_id = st.selectbox("Train id",
+                              sorted(X_train.train.unique()))
+    
+    station = st.selectbox("Station id",
+                           sorted(X_train.station.unique()))
+    
+    hour = st.time_input('Time slot')
+
+    composition = st.selectbox('Number of train units', [1, 2])
+    # Lags variables
+
+
