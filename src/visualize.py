b4200b424bee09ec2b03a77ed0c8b8c89d0e3211
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-20 21:13:15 -0400

    [ADD] new functions for the visualization in src/visualize

--- src/visualize.py
+++ src/visualize.py
@@ -3,7 +3,10 @@ import pandas as pd
 from src.sncf_dataset import SNCFDataset
 from src.load_data import load_data
 
+
 def price_date():
+    """Shows the average occupancy rates by days
+    """
     # Load datasets
     train_dataset, y_train, _ = load_data()
     train_dataset['occupancy_rate'] = y_train
@@ -19,7 +22,10 @@ def price_date():
     fig.autofmt_xdate()
     return fig
 
+
 def price_weekday():
+    """Shows the average occupancy rates by weekdays
+    """
     # Load datasets
     train_dataset = pd.read_pickle('datasets/X_train')
     train_dataset['occupancy_rate'] = pd.read_pickle('datasets/y_train')
@@ -33,15 +39,18 @@ def price_weekday():
         height=occupancy_rate_by_weekday,
         tick_label=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
         )
-    ax.set_title("Variation of occupancy rate by weekdays")
+    ax.set_title("Average occupancy rate by weekdays")
     ax.set_ylabel('Occupancy rate')
     return fig
 
+
 def price_hour():
+    """ Shows the average occupancy rates by hours
+    """
     # Load datasets
     train_dataset = pd.read_pickle('datasets/X_train')
     train_dataset['occupancy_rate'] = pd.read_pickle('datasets/y_train')
-    # Group by weekday
+    # Group by hours
     group_by_hours = train_dataset.groupby(by=['hour'])
     occupancy_rate_by_hours = group_by_hours.occupancy_rate.mean()
     fig, ax = plt.subplots(figsize=(3.,2.3))
@@ -49,4 +58,61 @@ def price_hour():
     ax.set_title("Variation of occupancy rate by hours")
     ax.set_xlabel('Hours')
     ax.set_ylabel('Occupancy rate')
+    return fig
+
+
+def price_composition():
+    """ Shows the average occupancy rates in relation with the number of train units
+    """
+    # Load datasets
+    train_dataset = pd.read_pickle('datasets/X_train')
+    train_dataset['occupancy_rate'] = pd.read_pickle('datasets/y_train')
+    # Group by composition
+    group_by_composition = train_dataset.groupby(by=['composition'])
+    occupancy_rate_by_train_units = group_by_composition.occupancy_rate.mean()
+    # Plots
+    fig, ax = plt.subplots()
+    ax.bar(
+        x=range(len(occupancy_rate_by_train_units)),
+        height=occupancy_rate_by_train_units,
+        tick_label=['Simple train unit', 'Double train unit']
+        )
+    ax.set_title("Average occupancy rate by composition")
+    ax.set_ylabel('Occupancy rate')
+    return fig
+
+
+def price_train_id():
+    """ Shows the histogram of average occupancy rates by train ids
+    """
+    # Load datasets
+    train_dataset, y_train, _ = load_data()
+    train_dataset['occupancy_rate'] = y_train
+    # Group by train ids
+    group_by_train_ids = train_dataset.groupby(by=['train'])
+    occupancy_rate_by_train_ids = group_by_train_ids.occupancy_rate.mean()
+    # Plots
+    fig, ax = plt.subplots()
+    ax.hist(occupancy_rate_by_train_ids, bins=20)
+    ax.set_title("Distribution of the occupancy rate by train ids")
+    ax.set_xlabel('Occupancy rate')
+    ax.set_ylabel('Number of train ids')
+    return fig
+
+
+def price_station():
+    """ Shows the histogram of average occupancy rates by stations
+    """
+    # Load datasets
+    train_dataset, y_train, _ = load_data()
+    train_dataset['occupancy_rate'] = y_train
+    # Group by weekday
+    group_by_stations = train_dataset.groupby(by=['station'])
+    occupancy_rate_by_stations = group_by_stations.occupancy_rate.mean()
+    # Plots
+    fig, ax = plt.subplots()
+    ax.hist(occupancy_rate_by_stations, bins=20)
+    ax.set_title("Distribution of the occupancy rate by stations")
+    ax.set_xlabel('Occupancy rate')
+    ax.set_ylabel('Number of stations')
     return fig
