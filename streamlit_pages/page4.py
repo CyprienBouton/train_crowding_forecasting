e365a1e9d143159e1903dfdbb43ef46ce2253f19
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-20 21:27:58 -0400

    [ADD] model selection in .\streamlit_pages\page4

--- streamlit_pages/page4.py
+++ streamlit_pages/page4.py
@@ -1,4 +1,23 @@
 import streamlit as st
 
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.svm import SVR
+from sklearn.tree import ExtraTreeRegressor, DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+
+
 def page4():
+
+    class_model_names = {
+        'Linear Model': LinearRegression,
+        'Lasso': Lasso,
+        'Decision Tree': DecisionTreeClassifier,
+        'Extra Tree Regressor': ExtraTreeRegressor,
+        'Random Forest': RandomForestRegressor,
+        'SVR': SVR
+    }
+    
     st.markdown("<h1>Occupancy rate forecasting</h1>", unsafe_allow_html=True)
+    model_used = st.selectbox("What model would you like to use?",
+                              class_model_names.keys())
