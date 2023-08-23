61526e3b106c13491fe9dec588b3e850bdd838ea
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-23 15:28:56 -0400

    [UP] src/preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,34 +10,13 @@ def datetime_to_float(d):
     return total_seconds
 
 
-def vectorize_columns(dataset, columns):
-    """ Vectorize the given columns.
-
-    :param dataset: Dataset on which transformation is applied
-    :type dataset: pandas.DataFrame
-    :param columns: Columns to vectorized.
-    :type columns: list of str or str
-    """
-    if isinstance(columns, str):
-        columns = [columns]
-    assert isinstance(columns, list)
-
-    # Looping on the columns to vectorized
-    for column in columns:
-
-        # Looping on the different values
-        for i, value in enumerate(dataset[column].unique()):
-            dataset[column+'_'+str(i+1)] = (dataset[column]==value).astype(float)
-
-    # Delete column
-    dataset.drop(columns, axis=1, inplace=True)
-
-
 def preprocessing(dataset):
     """ Convert non numerical values to numerical values
 
     :param dataset: dataset.
     :type datset: pandas.dataframe
+    :return dataset: Preprocessed dataset.
+    :rtype dataset: SNCFDataset
     """
     # convert dates to datetime
     dataset.date = pd.to_datetime(dataset.date)
@@ -50,12 +29,14 @@ def preprocessing(dataset):
     # Convert hour to float
     dataset.hour = pd.to_timedelta(dataset.hour).dt.components['hours']
 
-    # Vectorize station and train columns
-    vectorize_columns(dataset, ['station', 'train'])
+    # Vectorize train and station columns
+    dataset.train = dataset.train.astype(str) # convert train id to string
+    dataset = pd.get_dummies(dataset, dtype=float)
 
     # Delete constant column
     dataset.drop('way', axis=1, inplace=True)
 
     # Convert composition to float
     dataset.composition = dataset.composition.astype(float)
+    # return a SNCFDataset
     return SNCFDataset(dataset)
