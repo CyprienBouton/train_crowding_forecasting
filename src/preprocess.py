d2680e720fb49ddad7386a21539c73921cd5edd7
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-23 16:51:46 -0400

    [UP] src/preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,11 +10,19 @@ def datetime_to_float(d):
     return total_seconds
 
 
-def preprocessing(dataset):
+def get_train_columns():
+    # Get all columns of the train dataset
+    X_train = pd.read_pickle('datasets/X_train')
+    return X_train.columns
+
+
+def preprocessing(dataset, inference=True):
     """ Convert non numerical values to numerical values
 
     :param dataset: dataset.
     :type datset: pandas.dataframe
+    :param inference: Whether the processing is done during training or inference.
+    :type inference: bool
     :return dataset: Preprocessed dataset.
     :rtype dataset: SNCFDataset
     """
@@ -29,12 +37,20 @@ def preprocessing(dataset):
     # Convert hour to float
     dataset.hour = pd.to_timedelta(dataset.hour).dt.components['hours']
 
+    # Delete constant column
+    dataset = dataset.drop('way', axis=1)
+
     # Vectorize train and station columns
     dataset.train = dataset.train.astype(str) # convert train id to string
     dataset = pd.get_dummies(dataset, dtype=float)
-
-    # Delete constant column
-    dataset.drop('way', axis=1, inplace=True)
+    # Add missing columns during inference
+    if inference:
+        # Get missing columns
+        missing_cols = set(get_train_columns()) - set(dataset.columns)
+        for c in missing_cols:
+            dataset[c] = 0
+        # Ensure the order of column in the test set is in the same order than in train set
+        dataset = dataset[get_train_columns()]
 
     # Convert composition to float
     dataset.composition = dataset.composition.astype(float)
