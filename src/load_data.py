60bed519892924d919d2615db9eb956599b43212
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-23 17:55:15 -0400

    [UP] load_data adding a kwarg processed  in src/load_data

--- src/load_data.py
+++ src/load_data.py
@@ -1,20 +1,28 @@
 import pandas as pd
+import pickle
 
-def load_data():
+def load_data(processed=False):
     """Load all datasets: X, y and X_test
 
-    :param X: train dataset
-    :type X: pd.DataFrame
-    :param y: labels
-    :type y: pd.DataFrame
-    :param X_test: test dataset
-    :type X_test: pd.DataFrame
+    :param processed: Wether to return processed datasets
+    :return X: train dataset
+    :rtype X: pd.DataFrame
+    :return y: labels
+    :rtype y: pd.DataFrame
+    :return X_test: test dataset
+    :rtype X_test: pd.DataFrame
     """
-    X_train = pd.read_csv('datasets/Xtrain_hgcGIrA.csv')
-    y_train = pd.read_csv('datasets/Ytrain_yL5OjS4.csv')
-    X_test = pd.read_csv('datasets/Xtest.csv')
-
-    # Get rid of the row number
-    y_train.drop(columns='Unnamed: 0', inplace=True)
+    if processed:
+        X_train = pickle.open('datasets/X_train', 'rb')
+        y_train = pickle.open('datasets/y_train', 'rb')
+        X_test = pickle.open('datasets/X_test', 'rb')
+    
+    else:
+        X_train = pd.read_csv('datasets/Xtrain_hgcGIrA.csv')
+        y_train = pd.read_csv('datasets/Ytrain_yL5OjS4.csv')
+        X_test = pd.read_csv('datasets/Xtest.csv')
+        # Get rid of the row number
+        y_train.drop(columns='Unnamed: 0', inplace=True)
+    
     return X_train, y_train, X_test
 
