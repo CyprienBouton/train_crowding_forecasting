d5c2a14cffc6724446dfcff6f599a6e4dd4a6499
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-08-23 20:18:09 -0400

    [UP] page title in streamlit_pages/page4.py

--- streamlit_pages/page4.py
+++ streamlit_pages/page4.py
@@ -1,47 +1,65 @@
 import streamlit as st
+import pandas as pd
 from datetime import datetime
-
-from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.svm import SVR
-from sklearn.tree import ExtraTreeRegressor, DecisionTreeClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
+import numpy as np
+import pickle
 
 from src.load_data import load_data
+from src.model import predict_model, train_model
+from src.preprocess import preprocessing
+from src.sncf_dataset import SNCFDataset
 
 def page4():
-
-    X_train, _, _ = load_data()
-
-    class_model_names = {
-        'Linear Model': LinearRegression,
-        'Lasso': Lasso,
-        'Decision Tree': DecisionTreeClassifier,
-        'Extra Tree Regressor': ExtraTreeRegressor,
-        'Random Forest': RandomForestRegressor,
-        'SVR': SVR
-    }
-
+    # Set title
     st.markdown("<h1>Occupancy rate forecasting</h1>", unsafe_allow_html=True)
-    model_used = st.selectbox("What model would you like to use?",
-                              class_model_names.keys())
+    
+    # Get train and station columns
+    X_train, _, _ = load_data(processed=True)
+    X_train = SNCFDataset(X_train)   
+    train_columns, station_columns = X_train.trains_columns, X_train.stations_columns
+    input = {}
     
     # Contextual variables
-    date = st.date_input('Date of train passage', 
+    input['date'] = st.date_input('Date of train passage', 
                   value= datetime(2019,1,7), 
                   min_value=datetime(2019,1,7), 
                   max_value=datetime(2019,5,20), 
                   help="Date must be a weekday between January 7th and May 20th")
+    input['date'] = input['date'].strftime('%Y-%m-%d')
     
-    train_id = st.selectbox("Train id",
-                              sorted(X_train.train.unique()))
+    input['train'] = st.selectbox("Train id",
+                              train_columns)
     
-    station = st.selectbox("Station id",
-                           sorted(X_train.station.unique()))
+    input['station'] = st.selectbox("Station id",
+                           station_columns)
     
-    hour = st.time_input('Time slot')
+    input['hour'] = st.time_input('Time slot')
+    input['hour'] = input['hour'].strftime("%H:%M:%S")
 
-    composition = st.selectbox('Number of train units', [1, 2])
+    input['composition'] = st.selectbox('Number of train units', [1, 2])
+    
     # Lags variables
+    lag_variable_used = {}
+    for lag_variable in ['p1q0', 'p2q0', 'p3q0',
+                         'p0q1', 'p0q2', 'p0q3']:
+            
+            lag_variable_used[lag_variable] = st.checkbox(lag_variable + ' exist')
+            
+            input[lag_variable] = st.number_input(
+                lag_variable,
+                min_value=0.0,
+                max_value=1.0,
+                label_visibility="collapsed", 
+                disabled=not lag_variable_used[lag_variable])
+            
+            if not lag_variable_used[lag_variable]:
+                input[lag_variable] = np.nan
 
-
+    file = st.file_uploader("Import your model (.pkl format)",type='pkl')
+    model = pickle.load(file)
+    # Predict
+    if st.button('Predict'):
+            input['way'] = 0
+            input = preprocessing(pd.DataFrame(input, index=[0]))
+            st.write(str(predict_model(input, model)[0]))
+    
