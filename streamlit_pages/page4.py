0e868adcba3ba08f71e10aa2ff447830a621667a
CyprienBouton <cyprien.bouton@etu.minesparis.psl.eu>
2023-09-01 10:06:56 -0400

    [UP] streamlit_pages/page4.py

--- streamlit_pages/page4.py
+++ streamlit_pages/page4.py
@@ -8,6 +8,8 @@ from src.load_data import load_data
 from src.model import predict_model, train_model
 from src.preprocess import preprocessing
 from src.sncf_dataset import SNCFDataset
+from src.custom_buttons import numberNone_input
+
 
 def page4():
     # Set title
@@ -39,27 +41,37 @@ def page4():
     input['composition'] = st.selectbox('Number of train units', [1, 2])
     
     # Lags variables
-    lag_variable_used = {}
-    for lag_variable in ['p1q0', 'p2q0', 'p3q0',
-                         'p0q1', 'p0q2', 'p0q3']:
-            
-            lag_variable_used[lag_variable] = st.checkbox(lag_variable + ' exist')
-            
-            input[lag_variable] = st.number_input(
-                lag_variable,
-                min_value=0.0,
-                max_value=1.0,
-                label_visibility="collapsed", 
-                disabled=not lag_variable_used[lag_variable])
-            
-            if not lag_variable_used[lag_variable]:
-                input[lag_variable] = np.nan
+    st.markdown("<h4>Occupancy rate at the same station:</h4>", 
+                unsafe_allow_html=True)
+    columns_train = st.columns([1,1,1])
+    st.markdown("<h4>Occupancy rate of the same train:</h4>", 
+                unsafe_allow_html=True)
+    columns_station = st.columns([1,1,1])
+    for i, name in enumerate(["", "second ", "third "]):
+          with columns_train[i]:
+                input[f"p{i+1}q0"] = numberNone_input(
+                    name + "previous train",
+                    label_input=f'p{i+1}q0',
+                    min_value=0.0,
+                    max_value=1.0)
+                
+          with columns_station[i]:
+                input[f"p0q{i+1}"] = numberNone_input(
+                    name + "previous station",
+                    label_input=f"p0q{i+1}",
+                    min_value=0.0,
+                    max_value=1.0)
 
     file = st.file_uploader("Import your model (.pkl format)",type='pkl')
     # Predict
     if st.button('Predict'):
-            model = pickle.load(file)
-            input['way'] = 0
-            input = preprocessing(pd.DataFrame(input, index=[0]))
-            st.write(str(predict_model(input, model)[0]))
+            if not file:
+                st.error(f"🚨 You must first import a model!")
+            else:
+                model = pickle.load(file)
+                input['way'] = 0
+                input = preprocessing(pd.DataFrame(input, index=[0]))
+                prediction = predict_model(input, model)[0]
+                st.markdown(f"The occupancy rate is {round(prediction, 3)}", 
+                        unsafe_allow_html=True)
     
